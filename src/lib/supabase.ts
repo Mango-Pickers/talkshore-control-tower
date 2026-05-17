93842956e55feeb754ea517810386170861ca9b2
Arnold1AB <abrahamayebeng01@gmail.com>
2026-05-17 23:02:04 +0100

    Fixing supabase anon

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://xgoehmaxmavvujjasmvr.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhnb2VobWF4bWF2dnVqamFzbXZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODQ2MzYsImV4cCI6MjA5NDI2MDYzNn0.N3_MYx9NPkNKQzFkaw6Jex7ELLTWL195uNeTqxKtpAU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
