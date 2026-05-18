c347e93c6a1134c8406fbee2b327b4ea7e8e09c2
Arnold1AB <abrahamayebeng01@gmail.com>
2026-05-18 08:46:09 +0100

    Convert to Vite SPA

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#06152D",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "32px",
+      }}
+    >
+      TalkShore Control Tower
+    </div>
+  );
+}
